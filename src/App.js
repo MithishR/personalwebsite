30c6b5b18746eb4fa8a4951e0c59954e1a66484d
MithishR <mithishrg@gmail.com>
2024-07-16 17:10:51 -0400

    idk what i did but here we go

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+// import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      
+    //<div className="App">
+     <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact /> 
     </div>
   );
 }
