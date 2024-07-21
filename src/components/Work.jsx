0edf88ec7c8c7fe0e9fb1c2b4563d529af6ca0fc
MithishR <mithishrg@gmail.com>
2024-07-21 19:29:04 -0400

    added pics

---
+++ src/components/Work.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Work() {
+    return (
+        <div>
+            <h1>Work</h1>
+            <p>This is the Work page</p>
+        </div>
+    );
+}
+
+export default Work;
