16d46c27d902570d4ecb3222cfe0cb8c31cc5d70
MithishR <mithishrg@gmail.com>
2024-07-22 18:41:45 -0400

    trying to cook something

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { projects } from '../data/projects'; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { skillCategories } from '../data/skillCategories'; 
 
-function Projects() {
-    return (
-        <div>
-            <h1>Projects</h1>
-            <p>This is the projects page</p>
-        </div>
-    );
-}
+
+const Projects = () => {
+
+  return (
+    <div
+      name="project"
+      id="project"
+      className="h-screen w-full bg-[#000000] flex items-center justify-center pt-16 pb-8"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center w-full px-1 py-16">
+        <h2 className="text-4xl sm:text-7xl font-bold text-white mb-8">
+          Personal <span className="text-yellow-custom">Projects</span>
+        </h2>
+        <h3 className="text-xl font-semibold text-white mb-8">
+          Text!
+        </h3>
+    
+
+
+           
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { skillCategories } from '../data/skillCategories'; // Import from data folder
-
+import { skillCategories } from '../data/skillCategories'; 
 
 
 const Skills = () => {
